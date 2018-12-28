131f795a09e0e1fc3696b9a5c381ce958986ce97
qznp <cyysmr@163.com>
2018-12-29 00:25:12 +0800

    add koa2

---
+++ service/database/schema/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose') //引入Mongoose
+const Schema = mongoose.Schema //声明Schema
+const glob = require('glob')
+const {
+  resolve
+} = require('path')
+let ObjectId = Schema.Types.ObjectId //声明Object类型
+//创建我们的用户Schema
+const userSchema = new Schema({
+  UserId: ObjectId,
+  userName: {
+    unique: true,
+    type: String
+  },
+  password: String,
+  createAt: {
+    type: Date,
+    default: Date.now()
+  },
+  lastLoginAt: {
+    type: Date,
+    default: Date.now()
+  }
+})
+//发布模型
+mongoose.model('User', userSchema)
--- service/index.js
+++ service/index.js
@@ -1,4 +1,19 @@
 const Koa = require("koa");
 const app = new Koa();
+//引入connect
+const {
+  connect
+} = require('./database/init.js')
+//立即执行函数
+;
+(async () => {
+  await connect()
+})()
+app.use(async (ctx) => {
+  ctx.body = "<h1>hello</h1>"
+});
 
-app.use();
+app.listen(3000, function () {
+  console.log('3000');
+
+})
